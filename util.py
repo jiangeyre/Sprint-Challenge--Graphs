78e1e55ceb197bdbffd098b08fccbedb58524cb3
Lexie Jiang <lexiemingjiang@gmail.com>
2020-05-14 21:53:12 -0700

    Commit 2: I think I have it set up correctly... trying to troubleshoot. Will pick up again tomorrow

--- util.py
+++ util.py
@@ -1,5 +1,3 @@
-
-# Note: This Queue class is sub-optimal. Why?
 class Queue():
     def __init__(self):
         self.queue = []
@@ -26,3 +24,45 @@ class Stack():
     def size(self):
         return len(self.stack)
 
+def bfs(graph, starting_vertex):
+    visited_vertices = set()
+    queue = Queue()
+    queue.enqueue([starting_vertex])
+    while queue.size() > 0:
+        current_path = queue.dequeue()
+
+        current_vertex = current_path[-1]
+        # print("current_vertex " + str(current_vertex))
+        if current_vertex not in visited_vertices:
+            neighbors = get_neighbors(graph, current_vertex)
+            for neighbor in neighbors:
+                new_path = list(current_path)
+                new_path.append(neighbor)
+                queue.enqueue(new_path)
+                if neighbor == '?':
+                    return new_path
+            visited_vertices.add(current_vertex)
+
+
+def dfs(graph, starting_vertex):
+    visited_vertices = set()
+    stack = Stack()
+    stack.push([starting_vertex])
+    while stack.size() > 0:
+        current_path = stack.pop()
+
+        current_vertex = current_path[-1]
+        # print("current_vertex " + str(current_vertex))
+        if current_vertex not in visited_vertices:
+            neighbors = get_neighbors(graph, current_vertex)
+            for neighbor in neighbors:
+                new_path = list(current_path)
+                new_path.append(neighbor)
+                stack.push(new_path)
+                if neighbor == '?':
+                    return new_path
+            visited_vertices.add(current_vertex)
+
+# get the IDs of rooms or unknown
+def get_neighbors(graph, room):
+    return list(graph[room].values())
